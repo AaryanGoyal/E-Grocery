71c813f1a633ed8e2d32e3dd8d99153b802ea3b9
Aaryan Goyal <AaryanGoyal@users.noreply.github.com>
2020-06-24 23:55:51 +0530

    signup done login error

--- PythonApplication1/PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -2,10 +2,10 @@ from tkinter import *
 import mysql.connector
 
 mydb = mysql.connector.connect(
-  host="remotemysql.com",
-  user="dIDu0XCFUx",
-  password="0b0tyuI5rw",
-  database="dIDu0XCFUx")
+    host="remotemysql.com",
+    user="dIDu0XCFUx",
+    password="0b0tyuI5rw",
+    database="dIDu0XCFUx")
 
 print(mydb)
 
@@ -23,25 +23,29 @@ def execute_query(connection,query,args):
 class Welcome():
 
     def __init__(self,master):
-
         self.master=master
         self.master.geometry('400x150')  
         self.master.title('WELCOME')
-        self.button1 = Button(self.master, text="Login", command=self.gotoSignup).grid(row=4, column=0)
+        self.button1 = Button(self.master, text="Login", command=self.gotoLogin).grid(row=4, column=0)
         self.button2 = Button(self.master, text="Signup", command=self.gotoSignup).grid(row=6, column=0)
 
     def gotoSignup(self):
         self.master.destroy()
-        root2=Toplevel(self.master)
-        signup=Signup(root2)
+        root1=Toplevel()
+        signup=Signup(root1)
 
-class Signup():
-    
+    def gotoLogin(self):
+        self.master.destroy()
+        root1=Toplevel()
+        login=Login(root1)
+
+
+class Login():
+
     def __init__(self,master):
-
         self.master=master
         self.master.geometry('400x150')  
-        self.master.title('Tkinter Signup Form - Details')
+        self.master.title('Login Page')
 
         self.numberLabel = Label(self.master, text="Number").grid(row=0, column=0)
         self.number_var= StringVar()
@@ -51,23 +55,64 @@ class Signup():
         self.password_var= StringVar()
         self.passwordEntry = Entry(self.master, textvariable=self.password_var, show="*").grid(row=1, column=1)
 
+        self.loginButton = Button(self.master, text="Log In", command=self.login).grid(row=6, column=3)
+
+    def login(self):
+        self.number=self.number_var.get()
+        self.password=self.password_var.get()
+        print(self.number,self.password)
+        
+        query="""SELECT Password FROM LoginData where Number=?"""
+        args=(self.number)
+        execute_query(mydb,query,args)
+        password_check= cursor.fetchall()
+        if password_check==password:
+            print("succesful")
+        else:
+            print("unsuccesful")
+
+   
+
+
+class Signup():
+    
+    def __init__(self,master):
+
+        self.master=master
+        self.master.geometry('400x150')  
+        self.master.title('Signup Page')
+
+        self.numberLabel = Label(self.master, text="Number").grid(row=0, column=0)
+        self.number_var= StringVar()
+        self.numberEntry = Entry(self.master, textvariable=self.number_var).grid(row=0, column=1)
+
+        self.passwordLabel = Label(self.master,text="Password").grid(row=1, column=0)  
+        self.password_var= StringVar()
+        self.passwordEntry = Entry(self.master, textvariable=self.password_var, show="*").grid(row=1, column=1)
+
+        self.nameLabel = Label(self.master,text="Name").grid(row=2, column=0)  
+        self.name_var= StringVar()
+        self.nameEntry = Entry(self.master, textvariable=self.name_var).grid(row=2, column=1)
+
         self.type_var=StringVar(value="1")
-        self.customerButton=Radiobutton(self.master,text="Customer",value="customer",variable=self.type_var).grid(row=2, column=5)
-        self.shopkeeperButton=Radiobutton(self.master,text="Shopkeeper",value="shopkeeper",variable=self.type_var).grid(row=3, column=5)
+        self.customerButton=Radiobutton(self.master,text="Customer",value="customer",variable=self.type_var).grid(row=3, column=5)
+        self.shopkeeperButton=Radiobutton(self.master,text="Shopkeeper",value="shopkeeper",variable=self.type_var).grid(row=4, column=5)
 
         self.signupButton = Button(self.master, text="Sign up", command=self.signup).grid(row=6, column=3)
 
     def signup(self):
-        number=self.number_var.get()
-        password=self.password_var.get()
-        type=self.type_var.get()
-        print(number,password,type)
+        self.number=self.number_var.get()
+        self.password=self.password_var.get()
+        self.type=self.type_var.get()
+        self.name=self.name_var.get()
+        print(self.number,self.password,self.type,self.name)
     
-        query="""INSERT INTO LoginData (Number,Password,Type) 
-                             VALUES (%s,%s,%s)"""
-        args=(number,password,type)
+        query="""INSERT INTO LoginData (Number,Password,Type,Name) 
+                             VALUES (%s,%s,%s,%s)"""
+        args=(self.number,self.password,self.type,self.name)
         execute_query(mydb,query,args)
         return
+
 
 root=Tk()
 root.withdraw()
