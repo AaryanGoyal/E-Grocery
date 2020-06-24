d7221dbf1985758b2e13efac9ca1685717dc88e3
Aaryan Goyal <AaryanGoyal@users.noreply.github.com>
2020-06-24 10:12:05 +0530

    used classes

--- PythonApplication1/PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -19,34 +19,58 @@ def execute_query(connection,query,args):
         print(f"The error '{e}' occurred")
         return
 
-def signup():
-    number=number_var.get()
-    password=password_var.get()
-    type=type_var.get()
-    print(number,password,type)
+
+class Welcome():
+
+    def __init__(self,master):
+
+        self.master=master
+        self.master.geometry('400x150')  
+        self.master.title('WELCOME')
+        self.button1 = Button(self.master, text="Login", command=self.gotoSignup).grid(row=4, column=0)
+        self.button2 = Button(self.master, text="Signup", command=self.gotoSignup).grid(row=6, column=0)
+
+    def gotoSignup(self):
+        self.master.destroy()
+        root2=Toplevel(self.master)
+        signup=Signup(root2)
+
+class Signup():
     
-    query="""INSERT INTO LoginData (Number,Password,Type) 
-                         VALUES (%s,%s,%s)"""
-    args=(number,password,type)
-    execute_query(mydb,query,args)
-    return
+    def __init__(self,master):
 
-signuppage=Tk()  
-signuppage.geometry('400x150')  
-signuppage.title('Tkinter Signup Form - Details')
+        self.master=master
+        self.master.geometry('400x150')  
+        self.master.title('Tkinter Signup Form - Details')
 
-numberLabel = Label(signuppage, text="Number").grid(row=0, column=0)
-number_var= StringVar()
-numberEntry = Entry(signuppage, textvariable=number_var).grid(row=0, column=1)
+        self.numberLabel = Label(self.master, text="Number").grid(row=0, column=0)
+        self.number_var= StringVar()
+        self.numberEntry = Entry(self.master, textvariable=self.number_var).grid(row=0, column=1)
 
-passwordLabel = Label(signuppage,text="Password").grid(row=1, column=0)  
-password_var= StringVar()
-passwordEntry = Entry(signuppage, textvariable=password_var, show="*").grid(row=1, column=1)
+        self.passwordLabel = Label(self.master,text="Password").grid(row=1, column=0)  
+        self.password_var= StringVar()
+        self.passwordEntry = Entry(self.master, textvariable=self.password_var, show="*").grid(row=1, column=1)
 
-type_var=StringVar()
-customerButton=Radiobutton(signuppage,text="Customer",value="customer",variable=type_var).grid(row=2, column=5)
-shopkeeperButton=Radiobutton(signuppage,text="Shopkeeper",value="shopkeeper",variable=type_var).grid(row=3, column=5)
+        self.type_var=StringVar(value="1")
+        self.customerButton=Radiobutton(self.master,text="Customer",value="customer",variable=self.type_var).grid(row=2, column=5)
+        self.shopkeeperButton=Radiobutton(self.master,text="Shopkeeper",value="shopkeeper",variable=self.type_var).grid(row=3, column=5)
 
-signupButton = Button(signuppage, text="Sign up", command=signup).grid(row=6, column=0)
+        self.signupButton = Button(self.master, text="Sign up", command=self.signup).grid(row=6, column=3)
 
-signuppage.mainloop()
+    def signup(self):
+        number=self.number_var.get()
+        password=self.password_var.get()
+        type=self.type_var.get()
+        print(number,password,type)
+    
+        query="""INSERT INTO LoginData (Number,Password,Type) 
+                             VALUES (%s,%s,%s)"""
+        args=(number,password,type)
+        execute_query(mydb,query,args)
+        return
+
+root=Tk()
+root.withdraw()
+root1=Tk()
+welcome=Welcome(root1)
+root.mainloop()
