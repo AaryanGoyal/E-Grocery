c492af54866c9a27660c40c1db8dbadeb661178c
Aaryan Goyal <AaryanGoyal@users.noreply.github.com>
2021-09-07 04:19:56 +0530

    Final Complete

--- PythonApplication1/PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -4,15 +4,13 @@ import tkinter.ttk as ttk
 import mysql.connector
 import matplotlib.pyplot as plt
 from mysql.connector import Error
+from PIL import ImageTk,Image 
 mydb = mysql.connector.connect(
     host="remotemysql.com",
     user="V2wcYPWEoG",
     password="h3zp3K7DHj",
     database="V2wcYPWEoG")
 print(mydb)
-
-
-
 class Sql():
 
     def __init__(self,query,args):
@@ -23,19 +21,24 @@ class Sql():
         except Error as e:
             print("The error '{e}' occurred")
             return
-
-
-
 class Welcome():
 
     number=""
     name=""
     def __init__(self,master):
         self.master=master
-        self.master.geometry('400x150')  
+        self.master.geometry('250x150')  
         self.master.title('WELCOME')
-        self.button1 = Button(self.master, text="Login", command=self.gotoLogin).grid(row=0, column=1)
-        self.button2 = Button(self.master, text="Signup", command=self.gotoSignup).grid(row=0, column=0)
+        self.button1 = Button(self.master, text="Login", command=self.gotoLogin, width=15).grid(row=1, column=1, pady=6, padx=(5,4))
+        self.button2 = Button(self.master, text="Signup", command=self.gotoSignup, width=15).grid(row=1, column=0, pady=6, padx=(5,4))
+    
+        self.image1 = Image.open("Pngtreered_shopping_bags_315306.png")
+        self.resize_image=self.image1.resize((100,100))
+        self.image2 = ImageTk.PhotoImage(self.resize_image)
+        
+        self.img=Label(self.master, image=self.image2)
+        self.img.image=self.image2
+        self.img.grid(row=0, column=0, columnspan=2,padx=5,pady=5)
         
         def on_closing():
             if messagebox.askokcancel("Quit", "Do you want to quit?"):
@@ -58,25 +61,30 @@ class Welcome():
         root1=Toplevel()
         login=Login(root1)
         
-
-
 class Login():
 
     def __init__(self,master):
         self.master=master
-        self.master.geometry('400x150')  
-        self.master.title('Login Page')
+        self.master.geometry('220x225')  
+        self.master.title('Login')
 
-        self.numberLabel = Label(self.master, text="Number").grid(row=0, column=0)
+        self.numberLabel = Label(self.master, text="Number").grid(row=1, column=0 ,pady=5, padx=4,sticky='w')
         self.number_var= StringVar()
-        self.numberEntry = Entry(self.master, textvariable=self.number_var).grid(row=0, column=1)
+        self.numberEntry = Entry(self.master, textvariable=self.number_var).grid(row=1, column=1 )
 
-        self.passwordLabel = Label(self.master,text="Password").grid(row=1, column=0)  
+        self.passwordLabel = Label(self.master,text="Password").grid(row=2, column=0, sticky='w',pady=5, padx=4)  
         self.password_var= StringVar()
-        self.passwordEntry = Entry(self.master, textvariable=self.password_var, show="*").grid(row=1, column=1)
+        self.passwordEntry = Entry(self.master, textvariable=self.password_var, show="*").grid(row=2, column=1)
 
-        self.loginButton = Button(self.master, text="Log In", command=self.login).grid(row=2, column=1)
+        self.loginButton = Button(self.master, text="Log In", command=self.login, width=15).grid(row=3, column=1, pady=5)
 
+        self.image1 = Image.open("—Pngtree—user login or authenticate icon_5089976.png")
+        self.resize_image=self.image1.resize((100,100))
+        self.image2 = ImageTk.PhotoImage(self.resize_image)
+        
+        self.img=Label(self.master, image=self.image2)
+        self.img.image=self.image2
+        self.img.grid(row=0, column=1,padx=5,pady=5)
 
         def on_closing():
             if messagebox.askokcancel("Quit", "Do you want to quit?"):
@@ -86,7 +94,6 @@ class Login():
                 print("Exiting")
                 root.destroy()
         self.master.protocol("WM_DELETE_WINDOW", on_closing)
-
 
     def login(self):
         Welcome.number=self.number_var.get()
@@ -126,33 +133,38 @@ class Login():
         root1=Toplevel()
         shopkeeper=Shopkeeper(root1)
 
-
-
 class Signup():
     
     def __init__(self,master):
-
         self.master=master
-        self.master.geometry('400x150')  
-        self.master.title('Signup Page')
+        self.master.geometry('215x290')  
+        self.master.title('Signup')
 
-        self.numberLabel = Label(self.master, text="Number").grid(row=0, column=0)
+        self.numberLabel = Label(self.master, text="Number").grid(row=1, column=0,sticky='w',pady=5, padx=4)
         self.number_var= StringVar()
-        self.numberEntry = Entry(self.master, textvariable=self.number_var).grid(row=0, column=1)
+        self.numberEntry = Entry(self.master, textvariable=self.number_var).grid(row=1, column=1,sticky='w')
 
-        self.passwordLabel = Label(self.master,text="Password").grid(row=1, column=0)  
+        self.passwordLabel = Label(self.master,text="Password").grid(row=2, column=0,sticky='w',pady=5, padx=4)  
         self.password_var= StringVar()
-        self.passwordEntry = Entry(self.master, textvariable=self.password_var, show="*").grid(row=1, column=1)
+        self.passwordEntry = Entry(self.master, textvariable=self.password_var, show="*").grid(row=2, column=1,sticky='w')
 
-        self.nameLabel = Label(self.master,text="Name").grid(row=2, column=0)  
+        self.nameLabel = Label(self.master,text="Name").grid(row=3, column=0,sticky='w',pady=5, padx=4)  
         self.name_var= StringVar()
-        self.nameEntry = Entry(self.master, textvariable=self.name_var).grid(row=2, column=1)
+        self.nameEntry = Entry(self.master, textvariable=self.name_var).grid(row=3, column=1,sticky='w')
 
         self.type_var=StringVar(value="1")
-        self.customerButton=Radiobutton(self.master,text="Customer",value="customer",variable=self.type_var).grid(row=3, column=1)
-        self.shopkeeperButton=Radiobutton(self.master,text="Shopkeeper",value="shopkeeper",variable=self.type_var).grid(row=4, column=1)
+        self.customerButton=Radiobutton(self.master,text="Customer",value="customer",variable=self.type_var).grid(row=4, column=1,sticky='w')
+        self.shopkeeperButton=Radiobutton(self.master,text="Shopkeeper",value="shopkeeper",variable=self.type_var).grid(row=5, column=1,sticky='w')
 
-        self.signupButton = Button(self.master, text="Sign up", command=self.signup).grid(row=6, column=3)
+        self.signupButton = Button(self.master, text="Sign up", command=self.signup, width=15).grid(row=6, column=1)
+
+        self.image1 = Image.open("—Pngtree—user login or authenticate icon_5089976.png")
+        self.resize_image=self.image1.resize((100,100))
+        self.image2 = ImageTk.PhotoImage(self.resize_image)
+        
+        self.img=Label(self.master, image=self.image2)
+        self.img.image=self.image2
+        self.img.grid(row=0, column=1,padx=5,pady=5)
 
         def on_closing():
             if messagebox.askokcancel("Quit", "Do you want to quit?"):
@@ -191,19 +203,15 @@ class Signup():
         root1=Toplevel()
         shopkeeper=Shopkeeper(root1)
         return
-
-
-
 class Customer():
 
     def __init__(self,master):
         self.master=master
-        self.master.geometry('505x350')
-        self.master.title('Customer Page')
+        self.master.geometry('550x350')
+        self.master.title('Localé Bazar: '+Welcome.name)
 
         frame=LabelFrame(self.master, text='Place new order')
-        frame.grid(row=0,column=1)
-
+        frame.grid(row=0,column=1,pady=5)
 
         query="""SELECT Item FROM ItemData"""
         args=()
@@ -219,11 +227,11 @@ class Customer():
 
         self.quantityLabel = Label(frame,text="Quantity").grid(row=2, column=0)
         self.quantity_var= StringVar()          
-        self.quantityEntry = Entry(frame, textvariable=self.quantity_var)
+        self.quantityEntry = Entry(frame, textvariable=self.quantity_var,width=30)
         self.quantityEntry.grid(row=2, column=1)
 
         self.viewOrder()
-        self.orderButton = Button(frame, text="Place Order", command=self.placeOrder).grid(row=3, column=1)
+        self.orderButton = Button(frame, text="Place Order", command=self.placeOrder,width=20).grid(row=3, column=1)
 
         def on_closing():
             if messagebox.askokcancel("Quit", "Do you want to quit?"):
@@ -236,7 +244,7 @@ class Customer():
 
     def viewOrder(self):
         self.tree=ttk.Treeview(self.master)
-        self.tree.grid(row=6,column=1)
+        self.tree.grid(row=6,column=1,padx=20,pady=5)
 
         self.tree['column']=("column1","column2","column3","column4","column5")
         self.tree['show'] ='headings'
@@ -246,7 +254,6 @@ class Customer():
         self.tree.column("column3", width=100, minwidth=75)
         self.tree.column("column4", width=100, minwidth=75)
         self.tree.column("column5", width=100, minwidth=75)
-
 
         self.tree.heading("column1", text="S.NO",anchor=W)
         self.tree.heading("column2", text="Order ID",anchor=W)
@@ -264,7 +271,6 @@ class Customer():
             self.serial+=1
             self.tree.insert("","end",values=((self.serial),(i[0]),(i[2]),(i[3]),(i[4])))
 
-
     def placeOrder(self):
         self.item=self.itemchosen.get()
         self.quantity=self.quantity_var.get()
@@ -277,19 +283,23 @@ class Customer():
         self.itemchosen.current(0)
         self.quantityEntry.delete(0,'end')
         self.viewOrder()
-
-
-
-    
 class Shopkeeper():
     
     def __init__(self,master):
         self.master=master
-        self.master.geometry('400x150')  
-        self.master.title('WELCOME')
-        self.button1 = Button(self.master, text="View Orders", command=self.gotoOrder).grid(row=0, column=0)
-        self.button2 = Button(self.master, text="View Profit Summary", command=self.viewSummary).grid(row=1, column=0)
+        self.master.geometry('325x150')  
+        self.master.title('Localé Bazar: '+Welcome.name)
+        self.button1 = Button(self.master, text="View Orders", command=self.gotoOrder, width=20).grid(row=1, column=0, pady=6, padx=(5,4))
+        self.button2 = Button(self.master, text="View Profit Summary", command=self.viewSummary, width=20).grid(row=1, column=1, pady=6, padx=(5,4))
     
+        self.image1 = Image.open("unnamed.png")
+        self.resize_image=self.image1.resize((100,100))
+        self.image2 = ImageTk.PhotoImage(self.resize_image)
+        
+        self.img=Label(self.master, image=self.image2)
+        self.img.image=self.image2
+        self.img.grid(row=0, column=0, columnspan=2,padx=5,pady=5)
+
         def on_closing():
             if messagebox.askokcancel("Quit", "Do you want to quit?"):
                 print("SQL Connected=",mydb.is_connected())
@@ -299,13 +309,11 @@ class Shopkeeper():
                 root.destroy()
         self.master.protocol("WM_DELETE_WINDOW", on_closing)
     
-    
     def gotoOrder(self):
         root1=Toplevel()
         order=ShopView(root1)
 
     def viewSummary(self):
-    
         query="""SELECT OrderData.Item,OrderData.Quantity,ItemData.Price FROM OrderData INNER JOIN ItemData ON OrderData.Item=ItemData.Item WHERE OrderData.Completed=%s"""
         args=(Welcome.name,)
         summary=Sql(query,args)
@@ -328,17 +336,14 @@ class Shopkeeper():
         plt.xlabel("Items")
         plt.ylabel("total earnings")
         plt.show()
-
-
-
 class ShopView():
     
     def __init__(self,master):
         self.master=master
-        self.master.geometry('510x350')
+        self.master.geometry('550x360')
         self.master.title('View Order')
 
-        self.Label = Label(self.master, text="Hello "+Welcome.name+" These are the orders:-").grid(row=0, column=0)
+        self.Label = Label(self.master, text="Hello "+Welcome.name+" these are the orders:-",font=("Arial",17)).grid(row=0, column=0 ,padx=5,pady=5)
         self.viewOrder()
 
         frame=LabelFrame(self.master, text='Completed Order')
@@ -349,7 +354,6 @@ class ShopView():
         self.orderCompEntry = Entry(frame, textvariable=self.orderComp_var)
         self.orderCompEntry.grid(row=0, column=1)
         self.orderCompButton = Button(frame, text="Order Completed", command=self.OrderComp).grid(row=1, column=1)
-
     
     def OrderComp(self):
         self.orderComp_ID=self.orderComp_var.get()
@@ -364,7 +368,7 @@ class ShopView():
 
     def viewOrder(self):
         self.tree=ttk.Treeview(self.master)
-        self.tree.grid(row=1,column=0)
+        self.tree.grid(row=1,column=0,padx=20,pady=5)
 
         self.tree['column']=("column1","column2","column3","column4","column5")
         self.tree['show'] ='headings'
@@ -374,7 +378,6 @@ class ShopView():
         self.tree.column("column3", width=100, minwidth=75)
         self.tree.column("column4", width=100, minwidth=75)
         self.tree.column("column5", width=100, minwidth=75)
-
 
         self.tree.heading("column1", text="S.NO",anchor=W)
         self.tree.heading("column2", text="Order ID",anchor=W)
@@ -391,8 +394,6 @@ class ShopView():
         for i in orderDataDisplay:
             self.serial+=1
             self.tree.insert("","end",values=((self.serial),(i[0]),(i[1]),(i[2]),(i[3]+" {"+i[4]+"}")))
-
-
 class ShopSummary():
 
     def __init__(self,master):
@@ -400,11 +401,10 @@ class ShopSummary():
         self.master.geometry('500x150')
         self.master.title('View Summary')
 
-        self.Label = Label(self.master, text=("Hello "+Welcome.name+" This is the profit summary till now:-")).grid(row=0, column=0)
+        self.Label = Label(self.master, text=("Hello "+Welcome.name+" This is the profit summary till now:-"),font=("Arial",17)).grid(row=0, column=0)
         self.viewSummary()
 
     def viewSummary(self):
-
         query="""SELECT OrderData.Item,OrderData.Quantity,ItemData.Price FROM OrderData INNER JOIN ItemData ON OrderData.Item=ItemData.Item WHERE OrderData.Completed=%s"""
         args=(Welcome.name,)
         summary=Sql(query,args)
@@ -427,12 +427,9 @@ class ShopSummary():
         plt.xlabel("Items")
         plt.ylabel("total earnings")
         plt.show()
-        
 
 root=Tk()
 root.withdraw()
-root1=Tk()
+root1=Toplevel()
 welcome=Welcome(root1)
 root.mainloop()
-
-
